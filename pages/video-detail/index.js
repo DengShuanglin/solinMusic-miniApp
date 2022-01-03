4ba772e7030bf2c0a042477968830bd1f64d1fe0
Deng Shuanglin <shuanglindev@gmail.com>
2022-01-03 18:41:03 +0800

    feat: 完成视频播放页

--- pages/video-detail/index.js
+++ pages/video-detail/index.js
@@ -1,15 +1,53 @@
 // pages/video-detail/index.js
+import {
+  getMVUrl,
+  getMVDetail,
+  getRelatedVideoList,
+} from "../../service/api_video";
+
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    id: "",
+    urlInfo: {},
+    detail: {},
+    relatedList: [],
+
+    danmuList: [
+      {
+        text: "第 1s 出现的弹幕",
+        color: "#ff0000",
+        time: 1,
+      },
+      {
+        text: "第 3s 出现的弹幕",
+        color: "#ff00ff",
+        time: 3,
+      },
+    ],
+  },
+
+  getMvData(id) {
+    getMVUrl(id).then((res) => {
+      this.setData({ urlInfo: res.data });
+    });
+    getMVDetail(id).then((res) => {
+      this.setData({ detail: res.data });
+    });
+    getRelatedVideoList(id).then((res) => {
+      this.setData({ relatedList: res.data });
+    });
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
+    const { id = "" } = options;
+    this.setData({ id });
+    this.getMvData(id);
   },
 
   /**
--- service/api_video.js
+++ service/api_video.js
@@ -1,8 +1,43 @@
 import nativeRequest from "../service/index";
 
-export const getTopMvList = (offset, limit = 10) => {
+/**
+ * 获取mv列表
+ * @param {*} offset
+ * @param {*} limit
+ */
+export function getTopMvList(offset, limit = 10) {
   return nativeRequest.get("/top/mv", {
     offset,
     limit,
   });
-};
+}
+
+/**
+ * 根据id获取mv播放地址
+ * @param {*} id
+ */
+export function getMVUrl(id) {
+  return nativeRequest.get("/mv/url", {
+    id,
+  });
+}
+
+/**
+ * 根据id获取mv详情
+ * @param {*} mvid
+ */
+export function getMVDetail(mvid) {
+  return nativeRequest.get("/mv/detail", {
+    mvid,
+  });
+}
+
+/**
+ * 根据id获取相关联的视频列表
+ * @param {*} id
+ */
+export function getRelatedVideoList(id) {
+  return nativeRequest.get("/related/allvideo", {
+    id,
+  });
+}
